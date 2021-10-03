855e44c86aed9a0c1d81fd822d72b8859fadd35c
Markus Himmel <markus@himmel-villmar.de>
2021-10-03 15:04:48 +0200

    CI and license

--- src/main/java/com/github/twofx/datastructures/persistent/PersistentTreap.java
+++ src/main/java/com/github/twofx/datastructures/persistent/PersistentTreap.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2021 Markus Himmel
+ * This file is distributed under the terms of the Apache License, Version 2.0 
+ */
 package com.github.twofx.datastructures.persistent;
 
 public class PersistentTreap {
